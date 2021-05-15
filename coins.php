f0cad0024b4673f03fba6db276faa659925d9b8e
QQvanyusha <84239592+QQvanyusha@users.noreply.github.com>
2021-05-15 19:37:00 +0300

    Create coins.php

---
+++ coins.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title> 1</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="templatemo_style.css" rel="stylesheet" type="text/css" />
+<link href="css/jquery.ennui.contentslider.css" rel="stylesheet" type="text/css" media="screen,projection" />
+<script language="javascript" type="text/javascript">
+function clearText(field)
+{
+    if (field.defaultValue == field.value) field.value = '';
+    else if (field.value == '') field.value = field.defaultValue;
+}
+</script>
+</head>
+<body style="background:url(кс.png);
+background-size: 100%;-o-background-size: 100%;"
+> 
+
+    
+            <a href="#"><img src="основапенипукпук.png" alt=""/><span></span></a>
+        </div> <!-- end of site_title -->
+        <div id="templatemo_menu">
+            <ul>
+                
+                <li><a href="index.php">Игры</a></li>
+                <li><a href="portfolio.php">Связь</a></li>
+                <li><a href="blog.php">банк</a></li>
+                <li class="last"><a href="contact.php" class="current">Профиль</a></li>
+            </ul>
+            </div>
+            <div class="cleaner"></div>    	
+        </div>
+        <div class="cleaner"></div>
+    </div> <!-- end of header -->
+</div>
+<div id="templatemo_wrapper">  
+    
+    <div id="templatemo_content_top"></div>
+    
+    <div id="templatemo_content">
+    	<div id="templatemo_main_content">
+        
+        	<div class="content_box">
+                <h1></h1>
+                
+                <p></p>
+                
+              <div class="cleaner_h30"></div>
+            
+                <h4></h4>
+                <div class="col_w280">
+                    	<h3></h3>
+                         <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        
+            </div>
+                <div class="col_w280">
+                   		<h3></h3>
+                        <br />
+                         <br />
+                        <br />
+                        <br />
+                        <br />
+                        PROFIL
+            </div>
+                
+                <div class="cleaner"></div>
+			</div>
+            
+            <div class="content_box last_box">
+            	<div id="contact_form">
+                    <h4>LOGIN</h4>
+                    
+                    		
+    <form action="save_user.php" method="post">
+    <!--**** save_user.php - это адрес обработчика.  То есть, после нажатия на кнопку "Зарегистрироваться", данные из полей  отправятся на страничку save_user.php методом "post" ***** -->
+<p>
+    <label>Ваш логин:<br></label>
+    <input name="login" type="text" size="15" maxlength="15">
+    </p>
+<!--**** В текстовое поле (name="login" type="text") пользователь вводит свой логин ***** -->
+<p>
+    <label>Ваш пароль:<br></label>
+    <input name="ida" type="text" size="15" maxlength="15">
+    </p>
+<!--**** В поле для паролей (name="password" type="password") пользователь вводит свой пароль ***** --> 
+<p>
+    <input type="submit" name="submit" value="Зарегистрироваться">
+<!--**** Кнопочка (type="submit") отправляет данные на страничку save_user.php ***** --> 
+</p></form>
+                    
+                    </div>
+            </div>
+
+        </div>
+        
+        <div id="templatemo_sidebar">
+        
+        	<div id="news_box">
+                
+                <h2>24</h2>
+                        
+                <ul>
+                	<li>
+                        <h6><a href="#">25</a></h6>
+                        <p>26 <a href="http://validator.w3.org/check?uri=referer" rel="nofollow">27</a> &amp; <a href="http://jigsaw.w3.org/css-validator/check/referer" rel="nofollow">CSS</a> 28</p>
+                    </li>
+                    
+                    <li>
+                        <h6><a href="#">27</a></h6>
+                        <p>28</p>
+                    </li>
+                    
+                    <li class="last">
+                        <h6><a href="#">29</a></h6>
+                        <p>30 </p>
+                    </li>
+                    
+                </ul>
+                
+                <div class="cleaner"></div> 
+                    
+            </div>
+            
+            <div class="sidebar_box">
+            	<h2>31</h2>
+                <blockquote>
+                    <p align="justify"> 32</p>
+                    
+                    <cite>33 <span>34</span></cite>
+                </blockquote>
+            </div>
+            
+        </div>
+        
+        <div class="cleaner"></div>
+    </div>
+   
+    
+    
+     
+ <!-- end of wrapper -->
+<!-- templatemo 293 liquid -->
+<!-- 
+Liquid Template 
+http://www.templatemo.com/preview/templatemo_293_liquid 
+-->
+</body>
+</html>
+
