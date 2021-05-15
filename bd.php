6dcdba55db9360f0e78768336fcc51f75e301661
QQvanyusha <84239592+QQvanyusha@users.noreply.github.com>
2021-05-15 19:36:24 +0300

    Create bd.php

---
+++ bd.php
@@ -0,0 +1,7 @@
+<?php
+$host = 'localhost'; // адрес сервера 
+$database = 'casino'; // имя базы данных
+$user = 'root'; // имя пользователя
+$password = ''; // пароль
+$db = mysqli_connect ("localhost","root","","casino");
+?>
