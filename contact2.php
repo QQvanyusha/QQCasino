f0f2cb251cc46ffb5db5d1e0b65ed5126e1746cd
QQvanyusha <84239592+QQvanyusha@users.noreply.github.com>
2021-05-15 19:37:06 +0300

    Create contact2.php

---
+++ contact2.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title> 1</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="templatemo_style.css" rel="stylesheet" type="text/css" />
+<link href="css/jquery.ennui.contentslider.css" rel="stylesheet" type="text/css" media="screen,projection" />
+<script language="javascript" type="text/javascript">
+function clearText(field)
+{
+    if (field.defaultValue == field.value) field.value = '';
+    else if (field.value == '') field.value = field.defaultValue;
+}
+</script>
+</head>
+<body style="background:url(кс.png);
+background-size: 100%;-o-background-size: 100%;"
+> 
+
+    
+            <a href="#"><img src="основапенипукпук.png" alt=""/><span></span></a>
+        </div> <!-- end of site_title -->
+        <div id="templatemo_menu">
+            <ul>
+                
+                <li><a href="index.php">Игры</a></li>
+                <li><a href="portfolio.php">Связь</a></li>
+                <li><a href="blog.php">банк</a></li>
+                <li class="last"><a href="contact.php" class="current">Профиль</a></li>
+            </ul>
+            </div>
+            <div class="cleaner"></div>    	
+        </div>
+        <div class="cleaner"></div>
+    </div> <!-- end of header -->
+</div>
+<div id="templatemo_wrapper">  
+    
+    <div id="templatemo_content_top"></div>
+    
+    <div id="templatemo_content">
+    	<div id="templatemo_main_content">
+        
+
+            
+            <div class="content_box last_box">
+            	<div id="contact_form">
+                    <h4>LOGIN</h4>
+                    <img src=фотоtest.png  alt="фотоtest" alt="send" width="100px" height="100px"> <form enctype="multipart/form-data" method="post">
+   <p><input type="file" name="f">
+   <input type="submit" value="Отправить"></p>
+  </form> 
+                    <h4>Баланс: 0000000 <h4>
+
+
+                    
+                    		
+   
+            </div>
+
+        </div>
+        
+        <div id="templatemo_sidebar">
+        
+        	<div id="news_box">
+            <html>
+
+  
+</html>
+                
+                        
+                <ul>
+                	<li>
+                        <h6><a href="#">Последние игры</a></h6>
+                        <p>----------------------------------------------------------------------------------------------------------
+                    </li>
+                    
+                    <li>
+                        <h6><a href="#">27</a></h6>
+                        <p>28</p>
+                    </li>
+                    
+                    <li class="last">
+                        <h6><a href="#">29</a></h6>
+                        <p>30 </p>
+                    </li>
+                    
+                </ul>
+                
+                 
+               </div>
+        
+       
+    </div>
+   
+    
+    
+     
+ <!-- end of wrapper -->
+<!-- templatemo 293 liquid -->
+<!-- 
+Liquid Template 
+http://www.templatemo.com/preview/templatemo_293_liquid 
+-->
+</body>
+</html>
+
