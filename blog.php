9e1864c66810a279ed53ed73f93af731c5841cd5
QQvanyusha <84239592+QQvanyusha@users.noreply.github.com>
2021-05-15 19:36:37 +0300

    Create blog.php

---
+++ blog.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Liquid Blog Theme - free CSS template</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="templatemo_style.css" rel="stylesheet" type="text/css" />
+<link href="css/jquery.ennui.contentslider.css" rel="stylesheet" type="text/css" media="screen,projection" />
+<script language="javascript" type="text/javascript">
+function clearText(field)
+{
+    if (field.defaultValue == field.value) field.value = '';
+    else if (field.value == '') field.value = field.defaultValue;
+}
+</script>
+</head>
+<body style="background:url(cosmos.gif);
+background-size: 100%;-o-background-size: 100%;"> 
+
+        <div id="site_title">
+            <a href=""><img src="основапенипукпук.png" alt="" />
+            </a>
+
+        </div> <!-- end of site_title -->
+
+        <div id="templatemo_menu">
+              
+            <ul>
+               
+                <li><a href="index.php">Игры</a></li>
+                <li><a href="portfolio.php">Связь</a></li>
+                <li><a href="blog.php" class="current">Банк</a></li>
+                <li class="last"><a href="contact.php">Профиль</a></li>
+            </ul>
+                </form>
+            </div>
+           
+
+    
+    
+    	
+       
+
+    </div> <!-- end of templatemo_middle -->
+    
+    
+    
+    	
+         <!-- Credit: www.templatemo.com -->
+    
+    </div> <!-- end of templatemo_footer -->
+     
+</div> <!-- end of wrapper -->
+<!-- templatemo 293 liquid -->
+<!-- 
+Liquid Template 
+http://www.templatemo.com/preview/templatemo_293_liquid 
+-->
+</body>
+</html>
+
