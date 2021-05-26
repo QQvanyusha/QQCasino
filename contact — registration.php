a79b16d49d051ce0f6d362d7b35166e68ada7f9e
QQvanyusha <84239592+QQvanyusha@users.noreply.github.com>
2021-05-26 19:06:49 +0300

    Create contact — registration.php

---
+++ contact — registration.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title> Мой профиль</title>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<link href="templatemo_style.css" rel="stylesheet" type="text/css" />
+<link href="contact.css" rel="stylesheet" type="text/css" />
+<link href="css/jquery.ennui.contentslider.css" rel="stylesheet" type="text/css" media="screen,projection" />
+<script language="javascript" type="text/javascript">
+function clearText(field)
+{
+    if (field.defaultValue == field.value) field.value = '';
+    else if (field.value == '') field.value = field.defaultValue;
+}
+</script>
+</head>
+<body style="background:url(cosmos.gif);
+background-size: 100%;-o-background-size: 100%;"
+> 
+
+    
+            <a href="#"><img src="основапенипукпук.png" alt=""/><span></span></a>
+        </div> <!-- end of site_title -->
+        <div id="templatemo_menu">
+            <ul>
+                
+                <li><a href="index.php">Игры</a></li>
+                <li><a href="portfolio.php">Связь</a></li>
+                <li><a href="blog.php">банк</a></li>
+                <li class="last"><a href="contact.php" class="current">Профиль</a></li>
+            </ul>
+            </div>
+            <div class="cleaner"></div>    	
+        </div>
+        <div class="cleaner"></div>
+    
+
+
+        
+        	
+                <h1></h1>
+                
+                <p></p>
+                
+              <div class="cleaner_h30"></div>
+            
+                <h4></h4>
+                <div class="col_w280">
+                    	<h3></h3>
+                         <br />
+                        <br />
+                        <br />
+                        <br />
+                        <br />
+                        
+            </div>
+            <div class="login-box">
+  <h2>Login</h2>
+  <form>
+    <div class="user-box">
+      <input type="text" name="" required="">
+      <label>Username</label>
+    </div>
+    <div class="user-box">
+      <input type="password" name="" required="">
+      <label>Password</label>
+ 
+    </div>
+
+    <div class="user-box">
+      <input type="password" name="" required="">
+      <label>Password</label>
+ 
+    </div>
+
+    <div class="user-box">
+      <input type="password" name="" required="">
+      <label>Password</label>
+ 
+    </div>
+    <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Войти 
+    </a>
+    <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Зарегистрироваться
+    </a>
+  </form>
+</div>
+                
+        	
+                
+                
+   
+    
+    
+     
+ <!-- end of wrapper -->
+<!-- templatemo 293 liquid -->
+<!-- 
+Liquid Template 
+http://www.templatemo.com/preview/templatemo_293_liquid 
+-->
+</body>
+</html>
+
