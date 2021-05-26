d467e76388c5514a8e182fed381e1d8bb04252b3
QQvanyusha <84239592+QQvanyusha@users.noreply.github.com>
2021-05-26 19:38:49 +0300

    Update contact.php

--- contact.php
+++ contact.php
@@ -76,6 +76,7 @@ background-size: 100%;-o-background-size: 100%;"
       <span></span>
       <span></span>
       <li><a href="contact — registration.php">Зарегистрироваться</a></li> 
+      <a href="#">
     
   </form>
 </div>
