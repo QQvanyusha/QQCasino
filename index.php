6c14bb44423dc5b617c73a9f2f65f0893d2b0d78
Aleks-IT-lgtm <84493668+Aleks-IT-lgtm@users.noreply.github.com>
2021-05-29 18:17:33 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html PUBLIC ">
 <html>
 <head>
-<meta http-equiv="; charset=utf-8" />
+<meta http-equiv="; charset=utf-8" >
 <title>QuantCasino</title>
-<meta name="keywords" content="" />
+<meta name="keywords" content="" >
 <meta name="description" content="" />
-<link href="templatemo_style.css" rel="stylesheet" type="text/css" />
-<link href="css/jquery.ennui.contentslider.css" rel="stylesheet" type="text/css" media="screen,projection" />
+<link href="templatemo_style.css" rel="stylesheet" >
+<link href="css/jquery.ennui.contentslider.css" rel="stylesheet" type="text/css" media="screen,projection" >
 <script language="javascript" type="text/javascript">
 function clearText(field)
 {
@@ -17,44 +17,34 @@ function clearText(field)
 </head>  
 <body style="background:url(cosmos.gif);
 background-size: 100%;-o-background-size: 100%;">
+<div class="main-block">
+    <div id="site_title">
+        <b class="logo-a"href=""><img src="основапенипукпук.png" alt="" /><span></span></a>
+    </div> <!-- end of site_title -->
 
-
-
-<div id="site_title"></div>
-    <a href=""><img src="основапенипукпук.png" alt="" /><span></span></a>
-</div> <!-- end of site_title -->
-<div id="templatemo_menu">
-            <ul>
+    <div id="templatemo_menu">
+                  
+            <a href="about.php" class="current">Игры </a>
+            <a href="portfolio.php">Связь</a>
+            <a href="blog.php">Банк</a>
+            <a href="contact.php">Профиль</a>
+        
                 
-                
-                <li><a href="about.php" class="current">Игры </a></li>
-                <li><a href="portfolio.php">Связь</a></li>
-                <li><a href="blog.php">Банк</a></li>
-                <li class="last"><a href="contact.php">Профиль</a></li>
-            </ul>
             
-           
-                </form>
+                
+
+    <!-- end of header -->
+    </div>
+    <div style="display:flex;justify-content:center; width:100%">
+        <div id="hook" style="background-color: white; height:601px; width:1150px; display:flex;">
+
+            <div class="container">
+            <img src="sakura.jpg" alt="send" width="573.5px" height="601px" onclick="window.location.href = 'kosti/index.html'" />
+                <img src="unnamed.jpg" alt="send" width="572px" height="601px" onclick="window.location.href = 'Новая папка/index.html'" />          
             </div>
-              	
-        </div>
-  
-    </div> <!-- end of header -->
-</div>
-<div style="display:flex;justify-content:center; width:100%">
-
-<div id="hook" style="background-color: white; height:600px; width:1150px; display:flex;">
-
-    <div class="container">
-
-    <img src="sakura.jpg" alt="send" width="575px" height="600px" onclick="window.location.href = 'kosti/index.html'" />
-
-        <img src="unnamed.jpg" alt="send" width="574px" height="600px" onclick="window.location.href = 'Новая папка/index.html'" />
+        </div> 
 
     </div>
- 
-</div> 
-
 </div><!-- end of wrapper -->
 <!-- templatemo 293 liquid -->
 <!-- 
@@ -65,3 +55,8 @@ QuantCasino
 </body>
 </html>
 
+
+
+
+
+            
